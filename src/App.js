2f7adbc3223bdb4e0b5ca6330862ea7d1d596797
Cauxhy <youssefbouhaja1x@gmail.com>
2024-10-23 11:42:59 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-// src/App.js
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Projects from './Pages/Projects';
-import Certifications from './Pages/Certifications';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/certifications" element={<Certifications />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>Youssef BOUhaja</h1>
+    </div>
   );
 }
 
